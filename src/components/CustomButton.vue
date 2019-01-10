cf15ded84a71548e2c2446a7c81cbbcfbcce42ad
Sakhiri <mostafa.sakhiri@gmail.com>
2019-01-10 15:03:04 +0100

    Add custom button component

---
+++ src/components/CustomButton.vue
@@ -0,0 +1,22 @@
+<template>
+	<button :class="classes">{{text}}</button>
+</template>
+
+<script>
+export default {
+  name: 'custom-button',
+  props: {
+  	text: String,
+  	rounded: Boolean,
+  	classNames: String
+  },
+  computed: {
+  	classes() {
+  		if (this.rounded) {
+  			this.classNames += ' btn--rounded';
+  		}
+  		return `btn ${this.classNames}`
+  	}
+  }
+};
+</script>
