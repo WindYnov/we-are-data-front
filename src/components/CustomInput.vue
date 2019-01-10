9f3961f1af91d1e574f59dc59e2e83b782bafd5e
Sakhiri <mostafa.sakhiri@gmail.com>
2019-01-10 16:08:47 +0100

    Create custom input component

---
+++ src/components/CustomInput.vue
@@ -0,0 +1,18 @@
+<template>
+	<input :class="classes"/>
+</template>
+
+<script>
+export default {
+  name: 'custom-input',
+  props: {
+  	text: String,
+  	classNames: String
+  },
+  computed: {
+  	classes() {
+  		return `input ${this.classNames}`
+  	}
+  }
+};
+</script>
