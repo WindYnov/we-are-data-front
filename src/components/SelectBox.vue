aed69936b0b3c655b0fec6c6f27d81b7ec943a68
Sakhiri <mostafa.sakhiri@gmail.com>
2019-01-10 16:55:21 +0100

    Create selectbox component

---
+++ src/components/SelectBox.vue
@@ -0,0 +1,19 @@
+<template>
+	<select :class="classes">
+    <option class="select-box-option" v-for="option in options">{{option.label}}</option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: 'select-box',
+  props: {
+  	options: Array
+  },
+  computed: {
+  	classes() {
+  		return `select-box ${this.classNames}`
+  	}
+  }
+};
+</script>
