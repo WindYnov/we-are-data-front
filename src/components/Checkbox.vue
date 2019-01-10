bad6f19a107ddc695debc3ada19e4692d7280c5a
Sakhiri <mostafa.sakhiri@gmail.com>
2019-01-10 18:07:27 +0100

    Create checkbox component

---
+++ src/components/Checkbox.vue
@@ -0,0 +1,18 @@
+<template>
+	<label :class="`checkbox--container ${classNames}`">
+    <input type="checkbox">
+    <span @click="checked=!checked" class="checkbox--checkmark">
+      <span v-if="checked">✔</span>
+    </span>
+  </label>
+</template>
+
+<script>
+export default {
+  name: 'checkbox',
+  props: {
+    checked: Boolean,
+    classNames: String
+  }
+};
+</script>
