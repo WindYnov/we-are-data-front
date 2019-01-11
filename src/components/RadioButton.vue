faec7ca9dd96dd6db342a901146de6a27003f164
Sakhiri <mostafa.sakhiri@gmail.com>
2019-01-11 09:36:24 +0100

    Create radio button component

---
+++ src/components/RadioButton.vue
@@ -0,0 +1,18 @@
+<template>
+	<label :class="`radio--container ${classNames}`">
+    <input type="radio">
+    <span @click="checked=!checked" class="radio--checkmark">
+      <span class="radio--icon" v-if="checked"></span>
+    </span>
+  </label>
+</template>
+
+<script>
+export default {
+  name: 'radio',
+  props: {
+    checked: Boolean,
+    classNames: String
+  }
+};
+</script>
