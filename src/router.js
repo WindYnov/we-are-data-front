f244dcce99258510847cf3175a742386b2c9b692
mostafa-sakhiri <mostafa.sakhiri@gmail.com>
2019-01-18 10:07:53 +0100

    Fix router

--- src/router.js
+++ src/router.js
@@ -1,32 +1,51 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
-import Styleguide from './views/Styleguide.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
+import Styleguide from "./views/Styleguide.vue";
+import Login from "./views/Login.vue";
+import Dashboard from "./views/Dashboard.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/about",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/About.vue")
     },
     {
-      path: '/styleguide',
-      name: 'styleguide',
+      path: "/styleguide",
+      name: "styleguide",
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: Styleguide
+    },
+    {
+      path: "/login",
+      name: "login",
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: Login
+    },
+    {
+      path: "/dashboard",
+      name: "dashboard",
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: Dashboard
     }
   ]
-})
+});
