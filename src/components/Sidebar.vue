759b327aa45d0af808613c28120a0c06db09f6d9
mostafa-sakhiri <mostafa.sakhiri@gmail.com>
2019-01-18 10:07:29 +0100

    Create sidebar component

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="sidebar">
+    <a  v-for="setting in companySettings" class="setting" :href="setting.link">{{setting.title}}</a>
+  </div>
+</template>
+
+<script>
+// Import components
+export default {
+  name: 'sidebar',
+  data: () => {
+  return {
+    companySettings: [
+      { title: 'Personal settings', link: '' },
+      { title: 'Sales', link: '/sales' },
+      { title: 'Clients', link: '/clients' }
+    ]
+  };
+  }, methods: {
+  }
+}
+</script>
