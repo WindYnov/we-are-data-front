55e5468a2be4af63fa1c8cb74466336fa129fb3b
mostafa-sakhiri <mostafa.sakhiri@gmail.com>
2019-01-25 09:20:25 +0100

    Fix selectbox

--- src/components/SelectBox.vue
+++ src/components/SelectBox.vue
@@ -1,6 +1,7 @@
 <template>
-	<select :class="classes">
-    <option class="select-box-option" v-for="option in options">{{option.label}}</option>
+	<select v-model="value" :class="classes" @click="onSelectOption(value)">
+    <option class="select-box-option" disabled :value="defaultOption">{{defaultOption.label}}</option>
+    <option class="select-box-option" v-for="option in options" :value="option">{{option.label}}</option>
   </select>
 </template>
 
@@ -8,12 +9,30 @@
 export default {
   name: 'select-box',
   props: {
-  	options: Array
+    options: Array,
+    defaultOption: Object
   },
+
+  data: () => {
+    return {
+      value: null
+    };
+  },
+
   computed: {
-  	classes() {
-  		return `select-box ${this.classNames}`
-  	}
+    classes() {
+      return `select-box ${this.classNames}`
+    }
+  },
+
+  methods: {
+    onSelectOption(option) {
+      option.key && this.$emit('onSelectOption', option);
+    }
+  },
+
+  mounted() {
+    this.value = this.defaultOption;
   }
 };
 </script>
