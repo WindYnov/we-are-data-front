e6859c738ddd521f7196300324358c3a7f1aea17
mostafa-sakhiri <mostafa.sakhiri@gmail.com>
2019-02-07 21:10:20 +0100

    Create AddClients form component

---
+++ src/components/AddClientsForm.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="add-client-form">
+      <div class="close" @click="$emit('closeForm')">x</div>
+      <h3>Ajouter un client</h3>
+      <div>
+         <input v-model="client.nom" placeholder="Nom" type="text" >
+         <input v-model="client.secteur" placeholder="Secteur" type="text" >
+         <input v-model="client.siret" placeholder="Siret" type="text" >
+         <input v-model="client.mail" placeholder="Mail" type="mail" >
+         <input v-model="client.telephone" placeholder="Numéro" type="number" >
+         <div>
+            <CustomButton @click.native="saveClient(client) && resetForm()" classNames="btn--lg btn--green tu" text="Ajouter le client" />
+         </div>
+      </div>
+  </div>
+</template>
+
+
+<script>  
+// Import modules
+import { mapActions } from 'vuex';
+// Import components
+import CustomButton from '@/components/CustomButton.vue';
+
+export default {
+  name: 'add-client-form',
+  components: {
+    CustomButton
+  },
+  data: () => {
+    return {
+      client: {}
+    };
+  },
+  methods: {
+    ...mapActions(['saveClient']),
+    resetForm() {
+      this.client = {};
+    }
+  }
+};
+</script>
+
+<style>
+</style>
--- src/views/Clients.vue
+++ src/views/Clients.vue
@@ -1,38 +1,32 @@
 <template>
   <div class="clients">
-    <h3>Ajouter un client</h3>
-    <div class="add-client-form">
-       <input v-model="client.nom" placeholder="Nom" type="text" >
-       <input v-model="client.secteur" placeholder="Secteur" type="text" >
-       <input v-model="client.adress" placeholder="Adress" type="text" >
-       <input v-model="client.siret" placeholder="Siret" type="text" >
-       <input v-model="client.mail" placeholder="Mail" type="mail" >
-       <input v-model="client.numero" placeholder="Numéro" type="number" >
-       <div>
-          <CustomButton @click.native="saveClient(client)" classNames="btn--lg btn--green tu" text="Ajouter le client" />
-       </div>
+    <AddClientsForm v-if="showAddClientForm" v-on:closeForm="showAddClientForm = false;" />
+    <CustomButton id="add-client-btn" @click.native="showAddClientForm = true;" classNames="btn--lg btn--green tu" text="Ajouter des clients" />
+
+    <div class="clients-infos">
+      <input v-model="search" placeholder='Search for a client' type="search">
+      <Table
+        :data="clients"
+        :columns="gridColumns"
+        :filter-key="search"/>
     </div>
-    <h3>Liste des clients</h3>
-    <input v-model="search" placeholder='search' type="search">
-    <Table
-      :data="clients"
-      :columns="gridColumns"
-      :filter-key="search"/>
   </div>
 </template>
 
 <script>
 // Import modules
-import { mapState, mapActions } from 'vuex';
+import { mapState } from 'vuex';
 // Import components
 import Table from '@/components/Table.vue';
 import CustomButton from '@/components/CustomButton.vue';
+import AddClientsForm from '@/components/AddClientsForm.vue';
 
 export default {
   name: 'clients',
   components: {
     Table,
-    CustomButton
+    CustomButton,
+    AddClientsForm
   },
   computed: {
     ...mapState(['clients'])
@@ -40,11 +34,10 @@ export default {
   data: () => {
   return {
     search: '',
-    gridColumns: ['nom', 'secteur', 'siret', 'mail','numero'],
+    gridColumns: ['nom', 'secteur', 'siret', 'mail','telephone'],
+    showAddClientForm: false,
     client: {}
   };
-  }, methods: {
-    ...mapActions(['saveClient'])
   }
 };
 </script>
